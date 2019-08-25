3732514627071a7115cb680270fad738ac41a049
flyaway1217 <flyaway1217@gmail.com>
2019-08-25 11:03:32 -0600

    Inital Commit

---
+++ content/pages/cv.md
@@ -0,0 +1,47 @@
+Author: Yichu Zhou
+Title: Resume
+Date: 2019-01-14
+Slug: Resume
+
+# Publications
+
+- **[Beyond Context: A New Perspective for Word Embeddings][]** 
+    *Yichu Zhou* and Vivek Srikumar.  Proceedings of the Eighth Joint Conference on Lexical and Computational Semantics (* SEM 2019)
+
+- **[Building a Chinese Dependency GraphBank][]**
+    Bin Li, Yuan Wen, Cuijuan Xing, *Yichu Zhou* and Dekuan Xu. IEEE/WIC/ACM International Conference on Web Intelligence Workshops (WIW) 2016 Oct 13 (pp. 9-12). IEEE.
+
+- **[Resolving Coordinate Structures for Chinese Constituent Parsing]**
+    *Yichu Zhou*, Shujian Huang, Xinyu Dai and Jiajun Chen. Proceedings of the 4th CCF Conference on Natural Language Processing and Chinese Computing-Volume 9362
+
+# Experience
+
+- Oral Presentation in Starsem 2019
+- Teaching Assistant in Machine Learning Class Fall 2018
+- Teaching Assistant in Machine Learning Class Fall 2017
+
+# Educations
+
+- Ph.D. Candidate in Computer Science. [University of Utah][] (2016-Present)
+- M.S. in Computer Science. [Nanjing University][] (2013-2016)
+- B.S. in Computer Science. [Nanjing Normal University][] (2009-2013)
+
+# Softwares
+
+- [PYEVALB][] is a python version of [Evalb][] which is used to score the bracket tree banks.
+- [ExAssist][] is an light-weight assist tool that can save your time from doing experiments.
+- [FeVER][] is the implementation of the paper [Beyond Context: A New Perspective for Word Embeddings][].
+
+
+[Beyond Context: A New Perspective for Word Embeddings]: https://www.aclweb.org/anthology/S19-1#page=38
+[Building a Chinese Dependency GraphBank]: https://ieeexplore.ieee.org/abstract/document/7814465
+[Resolving Coordinate Structures for Chinese Constituent Parsing]: http://tcci.ccf.org.cn/conference/2015/papers/195.pdf
+[University of Utah]: https://www.utah.edu/
+[Nanjing University]: https://www.nju.edu.cn/
+[Nanjing Normal University]: http://www.njnu.edu.cn/
+
+[PYEVALB]: https://github.com/flyaway1217/PYEVALB
+[ExAssist]: http://exassist.zhouyichu.com/en/latest/
+[FeVER]: https://github.com/flyaway1217/FeVER
+[Evalb]: https://nlp.cs.nyu.edu/evalb/
+
---
+++ pelicanconf.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Yichu Zhou'
+SITENAME = "Yichu Zhou | Research Blog"
+SITEURL = ''
+
+PLUGIN_PATHS = ['../pelican-plugins']
+PLUGINS = ['post_stats', 'better_tables', 'tipue_search', 'neighbors', 'pelican-cite', 'render_math', 'simple_footnotes',  'pelican-toc', 'share_post']
+
+THEME='elegant'
+
+READING_TIME_LOWER_LIMIT = 1
+
+PATH = 'content'
+
+TIMEZONE = 'America/Denver'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('Github', 'https://github.com/flyaway1217'),
+        ('Email', 'flyaway1217@gmail.com', 'My Email Address'),
+        )
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+LANDING_PAGE_TITLE="Welcome to My Research Blog"
+
+PROJECTS = [{
+    'name': 'PYEVALB',
+    'url': 'https://github.com/flyaway1217/PYEVALB',
+    'description': 'is a python version of Evalb which is used to score the bracket tree banks.'},
+    {'name': 'ExAssist',
+    'url': 'http://exassist.zhouyichu.com/en/latest/',
+    'description': 'is an light-weight assist tool that can save your time from doing experiments.'},
+    {'name': 'FeVER', 
+        'url': 'https://github.com/flyaway1217/FeVER',
+        'description':'implementation of the paper Beyond Context: A New Perspective for Word Embeddings'
+        }]
+
+PROJECTS_TITLE = "My Projects"
+
+PUBLICATIONS_SRC = 'content/pubs.bib'
+
+SITE_LICENSE = """Content licensed under <a rel="license"
+    href="http://creativecommons.org/licenses/by/4.0/" target="_blank">
+    Creative Commons Attribution 4.0 International License</a>."""
+
+SITESUBTITLE = "Computers are useless. they can only give you answers."
+
+HOSTED_ON = {
+    "name": "Digital Ocean",
+    "url": "https://www.digitalocean.com/"
+    }
+
+ARTICLE_URL = '{slug}.html'
+
+AUTHORS = {
+    "Yichu Zhou": {
+        "url": "http://research.zhouyichu.com/",
+        "blurb": "is the owner of this blog.",
+    },
+}
+
+AUTHOR='Yichu Zhou'
+PDF_STYLE_PATH = 'output/pdf/'
